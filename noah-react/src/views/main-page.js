ff1860cb09b168bbf28c8f1647b6567f15de82a5
22amiibo <113854467+22amiibo@users.noreply.github.com>
2025-01-04 13:33:59 -0500

    Add files via upload

---
+++ noah-react/src/views/main-page.js
@@ -0,0 +1,289 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './main-page.css'
+
+const MainPage = (props) => {
+  return (
+    <div className="main-page-container10">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="main-page-main-page">
+        <div className="main-page-section1">
+          <div className="main-page-container11">
+            <span className="main-page-text10">
+              Welcome to Environmental Justice in Ivy City
+            </span>
+            <span className="main-page-text11">
+              Spreading awareness about NEP Building and environmental
+              injustices in Ivy City
+            </span>
+            <button className="main-page-button1">
+              <div className="main-page-primary1">
+                <span className="main-page-text12">Learn More</span>
+              </div>
+            </button>
+          </div>
+          <div className="main-page-imagecontainer1">
+            <div className="main-page-image1"></div>
+          </div>
+          <img
+            src="/external/vector2003015-6xg.svg"
+            alt="Vector2003015"
+            className="main-page-vector2001"
+          />
+        </div>
+        <div className="main-page-section2">
+          <div className="main-page-avatar1"></div>
+          <div className="main-page-container12">
+            <span className="main-page-text13">About Ivy City</span>
+            <span className="main-page-text14">
+              Learn about the history and demographics of Ivy City
+            </span>
+          </div>
+          <button className="main-page-button2">
+            <div className="main-page-primary2">
+              <span className="main-page-text15">Read More</span>
+            </div>
+          </button>
+          <img
+            src="/external/vector2003016-29fj.svg"
+            alt="Vector2003016"
+            className="main-page-vector2002"
+          />
+        </div>
+        <div className="main-page-contents">
+          <div className="main-page-container13">
+            <span className="main-page-text16">NEP Building Details</span>
+            <span className="main-page-text17">
+              Explore the harmful chemicals and health impacts associated with
+              the NEP Building
+            </span>
+          </div>
+          <div className="main-page-list1">
+            <div className="main-page-row1">
+              <div className="main-page-article1">
+                <div className="main-page-imagecontainer2">
+                  <div className="main-page-image2"></div>
+                </div>
+                <div className="main-page-frame4273189061">
+                  <span className="main-page-text18">Chemical Hazards</span>
+                  <span className="main-page-text19">
+                    Information on the toxic substances present in the NEP
+                    Building
+                  </span>
+                </div>
+              </div>
+              <div className="main-page-article2">
+                <div className="main-page-imagecontainer3">
+                  <div className="main-page-image3"></div>
+                </div>
+                <div className="main-page-frame4273189062">
+                  <span className="main-page-text20">Health Impact</span>
+                  <span className="main-page-text21">
+                    Discussing the health implications for the community
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            src="/external/vector2003018-qsm8.svg"
+            alt="Vector2003018"
+            className="main-page-vector2003"
+          />
+          <button className="main-page-button3">
+            <div className="main-page-primary3">
+              <span className="main-page-text22">Read More</span>
+            </div>
+          </button>
+        </div>
+        <div className="main-page-data-metrics">
+          <div className="main-page-container14">
+            <div className="main-page-container15">
+              <span className="main-page-text23">
+                Environmental Injustice Overview
+              </span>
+              <span className="main-page-text24">
+                Understanding the broader environmental issues in Washington,
+                D.C.
+              </span>
+            </div>
+          </div>
+          <div className="main-page-list2">
+            <div className="main-page-row2">
+              <div className="main-page-metric1">
+                <span className="main-page-text25">Pollution Levels</span>
+              </div>
+              <div className="main-page-metric2">
+                <span className="main-page-text26">Affected Communities</span>
+              </div>
+              <div className="main-page-metric3">
+                <span className="main-page-text27">Advocacy Efforts</span>
+                <span className="main-page-text28">Ongoing</span>
+              </div>
+            </div>
+          </div>
+          <div className="main-page-frame427318908"></div>
+          <div className="main-page-container16">
+            <div className="main-page-container17">
+              <span className="main-page-text29">
+                Will mandate the DOEE to implement the Environmental Justice
+                Index.
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+              <div className="main-page-frame427318907"></div>
+              <span className="main-page-text30">
+                Develop a structured approach to prevent organizations from
+                conducting activities in communities already facing
+                disproportionate environmental burdens.
+              </span>
+            </div>
+          </div>
+          <button className="main-page-button4">
+            <div className="main-page-primary4">
+              <span className="main-page-text31">Learn More</span>
+            </div>
+          </button>
+          <img
+            src="/external/vector2003022-9gtb.svg"
+            alt="Vector2003022"
+            className="main-page-vector2004"
+          />
+        </div>
+        <div className="main-page-reviews">
+          <div className="main-page-container18">
+            <span className="main-page-text32">Community Stories</span>
+            <span className="main-page-text33">
+              Voices from residents and activists
+            </span>
+          </div>
+          <div className="main-page-list3">
+            <div className="main-page-row3">
+              <div className="main-page-card1">
+                <div className="main-page-user1">
+                  <div className="main-page-avatar2">
+                    <div className="main-page-avatar3"></div>
+                    <div className="main-page-frame4273189063">
+                      <span className="main-page-text34">John Doe</span>
+                    </div>
+                  </div>
+                </div>
+                <span className="main-page-text35">
+                  Sharing personal experiences about environmental challenges
+                </span>
+              </div>
+              <div className="main-page-card2">
+                <div className="main-page-user2">
+                  <div className="main-page-avatar4">
+                    <div className="main-page-avatar5"></div>
+                    <div className="main-page-frame4273189064">
+                      <span className="main-page-text36">Jane Smith</span>
+                    </div>
+                  </div>
+                </div>
+                <span className="main-page-text37">
+                  Campaigning for a greener and healthier Ivy City
+                </span>
+              </div>
+            </div>
+          </div>
+          <img
+            src="/external/vector2003027-gie.svg"
+            alt="Vector2003027"
+            className="main-page-vector2005"
+          />
+        </div>
+        <div className="main-page-form">
+          <div className="main-page-container19">
+            <span className="main-page-text38">
+              Take Action Against the NEP Building
+            </span>
+            <span className="main-page-text39">
+              Join us in fighting for environmental justice
+            </span>
+          </div>
+          <div className="main-page-list4">
+            <div className="main-page-row4">
+              <div className="main-page-input1">
+                <span className="main-page-text40">Name</span>
+                <div className="main-page-textfield1">
+                  <span className="main-page-text41">Enter your name</span>
+                </div>
+                <span className="main-page-text42">
+                  Your name will not be shared
+                </span>
+              </div>
+            </div>
+            <div className="main-page-row5">
+              <div className="main-page-input2">
+                <span className="main-page-text43">Email</span>
+                <div className="main-page-textfield2">
+                  <span className="main-page-text44">Enter your email</span>
+                </div>
+                <span className="main-page-text45">
+                  We&apos;ll keep you updated
+                </span>
+              </div>
+            </div>
+            <button className="main-page-button5">
+              <div className="main-page-primary5">
+                <span className="main-page-text46">Submit</span>
+              </div>
+            </button>
+          </div>
+          <img
+            src="/external/vector2003024-ox6.svg"
+            alt="Vector2003024"
+            className="main-page-vector2006"
+          />
+        </div>
+        <div className="main-page-section3">
+          <div className="main-page-imagecontainer4">
+            <div className="main-page-image4">
+              <span className="main-page-text47">
+                Join us in making Ivy City a safer and cleaner place for all
+              </span>
+              <div className="main-page-pagination">
+                <img
+                  src="/external/rectangle33433028-u31p-200h.png"
+                  alt="Rectangle33433028"
+                  className="main-page-rectangle3343"
+                />
+                <img
+                  src="/external/rectangle33443028-xlvs-200h.png"
+                  alt="Rectangle33443028"
+                  className="main-page-rectangle3344"
+                />
+                <img
+                  src="/external/rectangle33473028-czo-200h.png"
+                  alt="Rectangle33473028"
+                  className="main-page-rectangle3347"
+                />
+                <img
+                  src="/external/rectangle33403028-ok6-200h.png"
+                  alt="Rectangle33403028"
+                  className="main-page-rectangle3340"
+                />
+              </div>
+            </div>
+          </div>
+          <img
+            src="/external/vector2003028-8b3p.svg"
+            alt="Vector2003028"
+            className="main-page-vector2007"
+          />
+        </div>
+        <div className="main-page-section4"></div>
+      </div>
+    </div>
+  )
+}
+
+export default MainPage
