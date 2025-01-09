0541553ebfef99fa3e172e3ee664a81b0b2dbca7
22amiibo <113854467+22amiibo@users.noreply.github.com>
2025-01-09 18:47:21 -0500

    Add files via upload

---
+++ bill-page.js
@@ -0,0 +1,427 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './bill-page.css'
+
+const BillPage = (props) => {
+  return (
+    <div className="bill-page-container1">
+      <Helmet>
+        <title>bill-Page - exported project</title>
+        <meta property="og:title" content="bill-Page - exported project" />
+      </Helmet>
+      <div className="bill-page-page">
+        <div className="bill-page-top-bar">
+          <div className="bill-page-navigation">
+            <span className="bill-page-text10">Home</span>
+          </div>
+          <span className="bill-page-text11">Contact</span>
+        </div>
+        <div className="bill-page-section">
+          <div className="bill-page-container2">
+            <span className="bill-page-text12">
+              The Environmental Justice Amendment Act of 2023
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="bill-page-text13">
+              To amend the District of Columbia Environmental Policy Act of 1989
+            </span>
+          </div>
+          <img
+            src="/external/vector2003443-fjxf.svg"
+            alt="Vector2003443"
+            className="bill-page-vector2001"
+          />
+        </div>
+        <div className="bill-page-products">
+          <div className="bill-page-imagecontainer10">
+            <div className="bill-page-image10"></div>
+          </div>
+          <div className="bill-page-container3">
+            <div className="bill-page-container4">
+              <span className="bill-page-text14">
+                What Bill (25-564) Will Do
+              </span>
+            </div>
+            <div className="bill-page-list10">
+              <div className="bill-page-row10">
+                <div className="bill-page-card1">
+                  <div className="bill-page-imagecontainer11">
+                    <div className="bill-page-image11">
+                      <span className="bill-page-text15">Icon</span>
+                      <div className="bill-page-tag1">
+                        <span className="bill-page-text16">Health</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bill-page-textcontent1">
+                    <span className="bill-page-text17">Amend</span>
+                    <span className="bill-page-text18">
+                      Better access and quality care
+                    </span>
+                  </div>
+                </div>
+                <div className="bill-page-card2">
+                  <div className="bill-page-imagecontainer12">
+                    <div className="bill-page-image12">
+                      <span className="bill-page-text19">Icon</span>
+                      <div className="bill-page-tag2">
+                        <span className="bill-page-text20">Environment</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bill-page-textcontent2">
+                    <span className="bill-page-text21">
+                      Environment Conservation
+                    </span>
+                    <span className="bill-page-text22">
+                      Reduce carbon emissions
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            src="/external/vector2003470-ww8q.svg"
+            alt="Vector2003470"
+            className="bill-page-vector2002"
+          />
+        </div>
+        <div className="bill-page-contents1">
+          <div className="bill-page-container5">
+            <span className="bill-page-text23">Benefits of the Bill</span>
+          </div>
+          <div className="bill-page-list11">
+            <div className="bill-page-row11">
+              <div className="bill-page-article10">
+                <div className="bill-page-frame42731890610">
+                  <span className="bill-page-text24">
+                    By enforcing accountability through penalties and legal
+                    actions, establishes a dedicated Environmental Justice
+                    Division within the DOEE, and prioritizes transparency and
+                    public participation in decision-making. Its ultimate goal
+                    is to prevent harmful projects from disproportionately
+                    impacting overburdened communities unless appropriate steps
+                    are taken to mitigate or offset those impacts.
+                  </span>
+                  <div className="bill-page-selection1">
+                    <div className="bill-page-labelnormal1">
+                      <span className="bill-page-text25">Summary</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <span className="bill-page-text26">
+            <span className="bill-page-text27">
+              Council of the District of Columbia. Environmental Justice
+              Amendment Act of 2023, B25-0564. Introduced November 6, 2023.
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span className="bill-page-text28">https://dccouncil.gov.</span>
+          </span>
+        </div>
+        <div className="bill-page-list12">
+          <div className="bill-page-row12">
+            <div className="bill-page-article11">
+              <div className="bill-page-imagecontainer13">
+                <div className="bill-page-image13"></div>
+              </div>
+              <div className="bill-page-frame42731890611">
+                <span className="bill-page-text29">
+                  Cumulative Impact Assessments
+                </span>
+                <span className="bill-page-text30">
+                  Mandates the preparation of a &quot;Cumulative Impact
+                  Statement&quot; (CIS) before granting permits for
+                  environmentally harmful activities in communities already
+                  facing heavy environmental burdens. These statements evaluate
+                  the combined environmental, social, and health effects of
+                  proposed projects.
+                </span>
+                <div className="bill-page-selection2">
+                  <div className="bill-page-labelnormal2">
+                    <span className="bill-page-text31">Permits (CIS)</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bill-page-list13">
+            <div className="bill-page-row13">
+              <div className="bill-page-article12">
+                <div className="bill-page-imagecontainer14">
+                  <div className="bill-page-image14"></div>
+                </div>
+                <div className="bill-page-frame42731890612">
+                  <span className="bill-page-text32">
+                    Modified Cumulative Impact Statements
+                  </span>
+                  <span className="bill-page-text33">
+                    Requires agencies to submit a &quot;Modified Cumulative
+                    Impact Statement&quot; for plans likely to cause significant
+                    environmental harm in overburdened areas.
+                  </span>
+                  <div className="bill-page-selection3">
+                    <div className="bill-page-labelnormal3">
+                      <span className="bill-page-text34">Modified (CIS)</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bill-page-list14">
+            <div className="bill-page-row14">
+              <div className="bill-page-article13">
+                <div className="bill-page-imagecontainer15">
+                  <div className="bill-page-image15"></div>
+                </div>
+                <div className="bill-page-frame42731890613">
+                  <span className="bill-page-text35">
+                    Enforcement and Accountability
+                  </span>
+                  <span className="bill-page-text36">
+                    Will grant power to the Mayor to impose penalties or revoke
+                    permits if applicants fail to implement the mitigating
+                    measures outlined in the CIS. Additionally, the People’s
+                    Counsel can file legal actions on behalf of affected
+                    residents.
+                  </span>
+                  <div className="bill-page-selection4">
+                    <div className="bill-page-labelnormal4">
+                      <span className="bill-page-text37">Enforcement</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bill-page-list15">
+            <div className="bill-page-row15">
+              <div className="bill-page-article14">
+                <div className="bill-page-imagecontainer16">
+                  <div className="bill-page-image16"></div>
+                </div>
+                <div className="bill-page-frame42731890614">
+                  <span className="bill-page-text38">
+                    Environmental Justice Division
+                  </span>
+                  <span className="bill-page-text39">
+                    Establishes a specialized division within the Department of
+                    Energy and Environment (DOEE) to focus on reducing
+                    environmental, energy, and climate-related challenges in
+                    vulnerable communities.
+                  </span>
+                  <div className="bill-page-selection5">
+                    <div className="bill-page-labelnormal5">
+                      <span className="bill-page-text40">New Division</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bill-page-list16">
+            <div className="bill-page-row16">
+              <div className="bill-page-article15">
+                <div className="bill-page-imagecontainer17">
+                  <div className="bill-page-image17"></div>
+                </div>
+                <div className="bill-page-frame42731890615">
+                  <span className="bill-page-text41">
+                    Identifying Overburdened Communities
+                  </span>
+                  <span className="bill-page-text42">
+                    Adopts the CDC&apos;s Environmental Justice Index (EJI) as a
+                    tool to pinpoint neighborhoods with high environmental and
+                    social vulnerabilities.
+                  </span>
+                  <div className="bill-page-selection6">
+                    <div className="bill-page-labelnormal6">
+                      <span className="bill-page-text43">Community</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bill-page-article16">
+              <div className="bill-page-imagecontainer18">
+                <div className="bill-page-image18"></div>
+              </div>
+              <div className="bill-page-frame42731890616">
+                <span className="bill-page-text44">
+                  Public Transparency and Engagement
+                </span>
+                <span className="bill-page-text45">
+                  Ensures that all CIS documents and related information are
+                  publicly accessible. Requires public hearings if significant
+                  public interest arises or if requested by a specific number of
+                  local voters.
+                </span>
+                <div className="bill-page-selection7">
+                  <div className="bill-page-labelnormal7">
+                    <span className="bill-page-text46">Public Engagement</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bill-page-row17">
+          <div className="bill-page-article17">
+            <div className="bill-page-imagecontainer19">
+              <div className="bill-page-image19"></div>
+            </div>
+            <div className="bill-page-frame42731890617">
+              <span className="bill-page-text47">
+                Blocking Projects with Disproportionate Impacts: 
+              </span>
+              <span className="bill-page-text48">
+                Prohibits the approval of projects with disproportionate
+                negative effects on overburdened communities unless effective
+                mitigating measures are applied or reasonable alternatives are
+                pursued.
+              </span>
+              <div className="bill-page-selection8">
+                <div className="bill-page-labelnormal8">
+                  <span className="bill-page-text49">Community</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bill-page-row18">
+          <div className="bill-page-article18">
+            <div className="bill-page-imagecontainer20">
+              <div className="bill-page-image20"></div>
+            </div>
+            <div className="bill-page-frame42731890618">
+              <span className="bill-page-text50">
+                Exemptions for Community-Benefiting Actions
+              </span>
+              <span className="bill-page-text51">
+                Exempts initiatives that aim to reduce disparities or directly
+                benefit overburdened areas, such as affordable housing, parks,
+                and healthcare facilities, from stringent CIS requirements.
+              </span>
+              <div className="bill-page-selection9">
+                <div className="bill-page-labelnormal9">
+                  <span className="bill-page-text52">Community</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bill-page-contents2">
+          <div className="bill-page-container6">
+            <span className="bill-page-text53">How You Can Help Pass It</span>
+          </div>
+          <div className="bill-page-list17">
+            <div className="bill-page-row19">
+              <div className="bill-page-article19">
+                <div className="bill-page-imagecontainer21">
+                  <div className="bill-page-image21"></div>
+                </div>
+                <div className="bill-page-frame42731890619">
+                  <span className="bill-page-text54">Sign a Petition</span>
+                  <span className="bill-page-text55">
+                    Show your support by signing our petition online
+                  </span>
+                </div>
+              </div>
+              <div className="bill-page-article20">
+                <div className="bill-page-imagecontainer22">
+                  <div className="bill-page-image22"></div>
+                </div>
+                <div className="bill-page-frame42731890620">
+                  <span className="bill-page-text56">
+                    Contact Your Representative
+                  </span>
+                  <span className="bill-page-text57">
+                    Send a message to your local representative urging their
+                    support
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            src="/external/vector2003488-pmkj.svg"
+            alt="Vector2003488"
+            className="bill-page-vector2003"
+          />
+        </div>
+        <div className="bill-page-container7">
+          <span className="bill-page-text58">Bill 25-564 Sponsors</span>
+        </div>
+        <div className="bill-page-list18">
+          <div className="bill-page-container8">
+            <span className="bill-page-text59">What Now?</span>
+            <span className="bill-page-text60">
+              Stay updated on the progress of the bill
+            </span>
+          </div>
+          <div className="bill-page-list19">
+            <div className="bill-page-row20">
+              <div className="bill-page-item1">
+                <div className="bill-page-frame1"></div>
+                <div className="bill-page-frame42731890621">
+                  <span className="bill-page-text61">Current Stage</span>
+                </div>
+                <span className="bill-page-text62">In Committee Review</span>
+                <img
+                  src="/external/vector2003410-irjm.svg"
+                  alt="Vector2003410"
+                  className="bill-page-vector2004"
+                />
+              </div>
+            </div>
+            <span className="bill-page-text63">Last active 2024-03-18</span>
+            <div className="bill-page-row21">
+              <div className="bill-page-item2">
+                <div className="bill-page-frame2">
+                  <span className="bill-page-text64">🎯</span>
+                </div>
+                <div className="bill-page-frame42731890622">
+                  <span className="bill-page-text65">Important Steps</span>
+                </div>
+                <img
+                  src="/external/vector2003410-haf.svg"
+                  alt="Vector2003410"
+                  className="bill-page-vector2005"
+                />
+              </div>
+            </div>
+          </div>
+          <span className="bill-page-text66">
+            Contact Your Ward Representatives
+          </span>
+          <img
+            src="/external/vector2003411-27lub.svg"
+            alt="Vector2003411"
+            className="bill-page-vector2006"
+          />
+        </div>
+        <img
+          src="/external/screenshot20250103at33037pm14130-pctq-300h.png"
+          alt="Screenshot20250103at33037PM14130"
+          className="bill-page-screenshot20250103at33037pm1"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default BillPage
